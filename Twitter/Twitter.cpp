ec8497f0898bfc881ae79676073d19b44ded71fb
Feranick <feranick@hotmail.com>
2011-10-08 23:03:29 -0400

    Added support for Arduino IDE v.1.0. The library works both for old and new Arduino IDE.

--- Twitter/Twitter.cpp
+++ Twitter/Twitter.cpp
@@ -1,33 +1,54 @@
 /*
   Twitter.cpp - Arduino library to Post messages to Twitter using OAuth.
   Copyright (c) NeoCat 2010. All right reserved.
+ 
+  Mofidied by feranick <feranick@hotmail.com> to add compatibility with Arduino IDE v.1.0
   
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
  */
 
-// ver1.2 - Use <string.h>
+// ver1.2 - Use <Udp.h> to support IDE 0019 or later
+// ver1.2.2_nf1 - Added support for Arduino IDE v.1.0
 
 #include <string.h>
 #include "Twitter.h"
 
 #define LIB_DOMAIN "arduino-tweet.appspot.com"
-static uint8_t server[] = {0,0,0,0}; // IP address of LIB_DOMAIN
 
+#if defined(ARDUINO) && ARDUINO < 100
+static uint8_t server[] = {0,0,0,0}; // IP address of LIB_DOMAIN
+#else
+static uint8_t *server; //IP address of LIB_DOMAIN
+#endif
+
+#if defined(ARDUINO) && ARDUINO < 100
 Twitter::Twitter(const char *token) : client(server, 80), token(token)
 {
 }
+#else
+Twitter::Twitter(const char *token) : client(*server), token(token)
+{
+}
+#endif
 
 bool Twitter::post(const char *msg)
-{
+{   
+    #if defined(ARDUINO) && ARDUINO < 100
 	DNSError err = EthernetDNS.resolveHostName(LIB_DOMAIN, server);
 	if (err != DNSSuccess) {
 		return false;
 	}
+#endif
+    
 	parseStatus = 0;
 	statusCode = 0;
+    #if defined(ARDUINO) && ARDUINO < 100
 	if (client.connect()) {
+    #else
+    if (client.connect(LIB_DOMAIN, 80)) {
+    #endif    
 		client.println("POST http://" LIB_DOMAIN "/update HTTP/1.0");
 		client.print("Content-Length: ");
 		client.println(strlen(msg)+strlen(token)+14);
--- Twitter/Twitter.h
+++ Twitter/Twitter.h
@@ -1,6 +1,8 @@
 /*
   Twitter.cpp - Arduino library to Post messages to Twitter using OAuth.
   Copyright (c) NeoCat 2010. All right reserved.
+ 
+  Mofidied by feranick <feranick@hotmail.com> to add compatibility with Arduino IDE v.1.0
   
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -8,6 +10,7 @@
 */
 
 // ver1.2 - Use <Udp.h> to support IDE 0019 or later
+// ver1.2.2_nf1 - Added support for Arduino IDE v.1.0
 
 #ifndef TWITTER_H
 #define TWITTER_H
@@ -18,7 +21,9 @@
 #include <SPI.h>
 #endif
 #include <Ethernet.h>
+#if defined(ARDUINO) && ARDUINO < 100
 #include <EthernetDNS.h>
+#endif
 
 class Twitter
 {
@@ -26,7 +31,11 @@ private:
 	uint8_t parseStatus;
 	int statusCode;
 	const char *token;
+    #if defined(ARDUINO) && ARDUINO < 100
 	Client client;
+    #else
+    EthernetClient client;
+    #endif
 public:
 	Twitter(const char *user_and_passwd);
 	
